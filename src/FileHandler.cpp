e3ae8dc99ad49de4a0edfc0c6286af548d8094b9
DaniilZinoviev06 <daniilzinoviev95@gmail.com>
2025-06-29 18:24:31 +0300

    some changes

--- src/FileHandler.cpp
+++ src/FileHandler.cpp
@@ -4,13 +4,21 @@
 
 class FileHandler {
 public:
-    FileHandler() {
+    FileHandler(const std::string &file_name)
+        : file(file_name) {
+        if(!file.is_open()) {
+            throw std::runtime_error("Ошибка - файл не открыт");
+        }
 
+        cout << "Файл открыт";
     }
 
 
     ~FileHandler() {
-
+        if(file.is_open()) {
+            file.close();
+            cout << "Файл закрыт";
+        }
     }
 
 
