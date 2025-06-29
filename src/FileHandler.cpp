d16031a883b64966b021d1512d0507942dee4944
DaniilZinoviev06 <daniilzinoviev95@gmail.com>
2025-06-29 14:12:38 +0300

    Initial commit

---
+++ src/FileHandler.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <fstream>
+#include <stdexcept>
+
+class FileHandler {
+public:
+    FileHandler() {
+
+    }
+
+
+    ~FileHandler() {
+
+    }
+
+
+    void readLine() {
+
+    }
+
+    void writeLine() {
+
+    }
+
+private:
+    std::ofstream file
+}
